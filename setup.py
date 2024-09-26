8aa9aecef3701a7a1ee29261747e7a5e47578446
qiaoyk <qiaoyk@geovis.com.cn>
2024-09-26 15:07:53 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="test_sign_api_python3",
+    version="0.0.1",
+    keywords=("pip", "tool"),
+    description="test description",
+    long_description="more description...",
+    license="MIT Licence",
+
+    url="https://github.com/qiaoyk666/test_sign_api_python3.git",
+    author="qiao",
+    author_email="979146919@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any"
+)
